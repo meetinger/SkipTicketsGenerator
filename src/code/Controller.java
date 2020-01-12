e327a44016d509cfb79e94c97ff06064706b3322
Yanchik0402 <shamil0402@list.ru>
2020-01-12 18:28:52 +0300

    Add FileChooser And DirectoryChooser

--- src/code/Controller.java
+++ src/code/Controller.java
@@ -4,14 +4,42 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.DirectoryChooser;
+
+import javax.sound.midi.Patch;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Controller {
-    @FXML
-    private Button qrhelpbtn;
 
     @FXML
-    public void onClickMethod(){
+    public void onClickMethod() {
         FxDialogs.showInformation("Помощь", "%number% - для кодирования номера талона, \n%name% - для кодирования ФИО, \nлибо просто введите любой текст.");
     }
 
+    @FXML
+    public void getFile() {
+        FileChooser fc = new FileChooser();
+        File photo = fc.showOpenDialog(null);
+
+        if (photo != null) {
+            FxDialogs.showInformation("Файл", photo.getPath());
+        } else {
+            FxDialogs.showError("Ошибка", "Файл Неверный!");
+        }
+    }
+
+    @FXML
+    public void getDir() {
+        DirectoryChooser dc = new DirectoryChooser();
+        File dirfile = dc.showDialog(null);
+
+        if (dirfile != null) {
+            FxDialogs.showInformation("Директория", dirfile.getPath());
+        } else {
+            FxDialogs.showError("Ошибка", "Директория Неверный!");
+        }
+    }
 }

f8c1ff78c96be104873159dc92c9ef4a0f58d622
Yanchik0402 <shamil0402@list.ru>
2020-01-13 22:43:03 +0300

    Add stamp support

--- src/code/Controller.java
+++ src/code/Controller.java
@@ -3,29 +3,84 @@ package code;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.DirectoryChooser;
 
 import javax.sound.midi.Patch;
+
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Controller {
 
+    ArrayList<Ticket> tickets = new ArrayList<>();
+
     @FXML
-    public void onClickMethod() {
+    TextField schoolField, fioField, amountField, qrcodeField;
+
+    @FXML
+    ImageView preImage;
+    String schoolName;
+    String fio;
+    int amount;
+    String qrcode;
+
+    Image image;
+
+    Ticket preView;
+
+
+    public void createTicket(String index, String school, String fio, String QRCode, Image teacher){
+        tickets.add(new Ticket(index, school,fio, QRCode, teacher));
+    }
+
+    public void updateVars(){
+        schoolName = schoolField.getText();
+        fio = fioField.getText();
+        amount = Integer.parseInt(amountField.getText());
+        qrcode = qrcodeField.getText();
+    }
+
+
+    @FXML
+    public void updatePreview(){
+        updateVars();
+        //System.out.println("UPDATED!");
+        preView = new Ticket("0", schoolName, fio, qrcode, image);
+        //System.out.println("CREATED!");
+        //FxDialogs.showInformation("Переменные", schoolName+"\n"+fio+"\n"+amount+"\n"+qrcode);
+       // System.out.println("SHOWED!");
+        preImage.setImage(preView.getResultImage());
+       // System.out.println("SETED!");
+    }
+
+
+    @FXML
+    public void showHelp() {
         FxDialogs.showInformation("Помощь", "%number% - для кодирования номера талона, \n%name% - для кодирования ФИО, \nлибо просто введите любой текст.");
     }
 
     @FXML
     public void getFile() {
         FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().addAll(//
+                new FileChooser.ExtensionFilter("All Files", "*.*"),
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png"),
+                new FileChooser.ExtensionFilter("GIF", "*.gif")
+        );
         File photo = fc.showOpenDialog(null);
 
         if (photo != null) {
             FxDialogs.showInformation("Файл", photo.getPath());
+            image = new Image(photo.toURI().toString());
+            //updatePreview(image);
         } else {
             FxDialogs.showError("Ошибка", "Файл Неверный!");
         }
@@ -42,4 +97,6 @@ public class Controller {
             FxDialogs.showError("Ошибка", "Директория Неверный!");
         }
     }
+
+    
 }
--- src/code/Ticket.java
+++ src/code/Ticket.java
@@ -1,18 +1,90 @@
 package code;
 
 
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+
 import java.awt.*;
+import java.awt.image.BufferedImage;
+
 
 public class Ticket {
 
     private String index, school, fio;
-    private Image QRCode, stamp, teacher;
+    private Image QRCode, stamp, teacher, resultImage;
 
-    public Ticket(String index, String school, String fio, String QRCode, Image teacher){
+
+    public Ticket(String index, String school, String fio, String QRCode, Image teacher) {
         this.index = index;
         this.school = school;
         this.fio = fio;
         //QRCODE
         this.teacher = teacher;
+        resultImage = new Image("/img/template.png");
+        stamp = new Image("/img/stamp_r.png");
+        writeStamp();
+        makeResultImage();
+    }
+   /* private Image QRGenerate(){
+        Image qr = new Image(null);
+
+    }
+*/
+
+    public Image getResultImage() {
+        return resultImage;
+    }
+
+   /* private void writeStamp() {
+        String[] fioArr = fio.split(" ");
+        BufferedImage stampAwp = SwingFXUtils.fromFXImage(stamp, null);
+        Graphics2D gr = (Graphics2D) stampAwp.getGraphics();
+        gr.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        gr.setColor(new Color(0, 85, 166));
+
+        gr.setFont(new Font("Arial", Font.BOLD, 50));
+        printCenterString(gr, fioArr[0].toUpperCase(), 300, 100, 200);
+        printCenterString(gr, fioArr[1], 300, 100, 250);
+        printCenterString(gr, fioArr[2], 300, 100, 300);
+
+        gr.drawImage(stampAwp.getScaledInstance(250, 250, 0), 0, 0, null);
+        BufferedImage tmp = stampAwp;
+
+        //tampAwp = stampAwp.getScaledInstance(250, 250, 0).getBufferedImage();
+
+        //gr.drawImage(stampAwp, 0, 250, 250, 0, null, null, stampAwp.getWidth(), stampAwp.getHeight());
+        stamp = SwingFXUtils.toFXImage(stampAwp, null);
+    }*/
+
+    private void writeStamp() {
+        String[] fioArr = fio.split(" ");
+        BufferedImage stampAwp = SwingFXUtils.fromFXImage(stamp, null);
+        Graphics2D gr = (Graphics2D) stampAwp.getGraphics();
+        gr.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        gr.setColor(new Color(0, 85, 166));
+
+        gr.setFont(new Font("Arial", Font.BOLD, 25));
+        printCenterString(gr, fioArr[0].toUpperCase(), 150, 50, 100);
+        for(int i = 1; i<fioArr.length;++i){
+            printCenterString(gr, fioArr[1], 150, 50, 100+i*25);
+        }
+
+        stamp = SwingFXUtils.toFXImage(stampAwp, null);
+    }
+
+
+    private void makeResultImage() {
+        BufferedImage resultAwp = SwingFXUtils.fromFXImage(resultImage, null);
+        Graphics2D gr = (Graphics2D) resultAwp.getGraphics();
+        gr.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        gr.drawImage(SwingFXUtils.fromFXImage(stamp, null), 1425, 45, null);
+        resultImage = SwingFXUtils.toFXImage(resultAwp, null);
+    }
+
+    private void printCenterString(Graphics gr, String str, int width, int X, int Y) {
+        int stringLen = (int)
+                gr.getFontMetrics().getStringBounds(str, gr).getWidth();
+        int start = width / 2 - stringLen / 2;
+        gr.drawString(str, start + X, Y);
     }
 }

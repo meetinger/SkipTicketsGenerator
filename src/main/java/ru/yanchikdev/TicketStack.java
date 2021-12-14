5a6fc3e2b310eacffd5d680c26d72822dbe67fc6
meetinger <meetingerdev@gmail.com>
2021-12-14 15:56:52 +0300

    Minor changes

--- src/main/java/ru/yanchikdev/TicketStack.java
+++ src/main/java/ru/yanchikdev/TicketStack.java
@@ -9,13 +9,13 @@ import java.util.ArrayList;
 public class TicketStack {
     ArrayList<Ticket> tickets = new ArrayList<Ticket>();
     BufferedImage toPrint;
-    int devider = 4;
+    int divider = 4;
     int imgH;
     int imgW;
 
     public TicketStack(ArrayList<Ticket> tickets) {
-        imgH = tickets.get(0).getResultImage().getHeight() * devider;
-        imgW = tickets.get(0).getResultImage().getWidth() * (devider-1);
+        imgH = tickets.get(0).getResultImage().getHeight() * divider;
+        imgW = tickets.get(0).getResultImage().getWidth() * (divider -1);
         toPrint = new BufferedImage(imgW, imgH, BufferedImage.TYPE_INT_RGB);
         this.tickets = tickets;
         this.writeTickets();
@@ -23,10 +23,10 @@ public class TicketStack {
 
     public void writeTickets() {
         Graphics2D gr = toPrint.createGraphics();
-        int height = imgH / devider;
-        int width = imgW / (devider-1);
+        int height = imgH / divider;
+        int width = imgW / (divider -1);
         for (int i = 0; i < tickets.size(); ++i) {
-            gr.drawImage(ImageUtils.fitByHeight(tickets.get(i).getResultImage(), height), width * (i / devider), height * (i % devider), null);
+            gr.drawImage(ImageUtils.fitByHeight(tickets.get(i).getResultImage(), height), width * (i / divider), height * (i % divider), null);
         }
     }
 
